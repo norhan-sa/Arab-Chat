7d40f82f061c87a11387000cedc314c84319c269
norhan-sa <norhan.sa200@gmail.com>
2020-10-27 05:42:13 +0200

    First Commit

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const   detect  =   require('./routes/detect');
 app.use(express.json());
 app.use('/',auth);
 app.use('/',detect);
+app.get('/',(req,res)=>{
+  return res.send('<h1><pre> W E L C O M E   T O  A R A B  C H A T </pre></h1>')
+});
 
 
 module.exports = app;
